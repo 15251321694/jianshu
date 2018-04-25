e5d9ac3353888ce2ee9dc0655440d3b79da61b0a
Your Name <you@example.com>
2018-04-25 20:31:46 +0800

    详情页面布局及评论

---
+++ layouts/sign.vue
@@ -0,0 +1,228 @@
+<template>
+  <div id="sign-template">
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background-color: #f1f1f1;
+}
+.sign {
+  height: 100%;
+  font-size: 14px;
+  text-align: center;
+  background-color: #f1f1f1;
+  min-height: 750px;
+}
+.sign:before {
+  width: 100%;
+  content: "";
+  display: inline-block;
+  height: 85%;
+  vertical-align: middle;
+  box-sizing: border-box;
+}
+.sign .logo {
+  position: absolute;
+  left: 50px;
+  top: 50px;
+}
+.sign img {
+  width: 100px;
+}
+.sign .main {
+  width: 400px;
+  margin: 80px auto 0;
+  padding: 50px 50px 30px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+  display: inline-block;
+  vertical-align: middle;
+}
+.sign .main .title {
+  margin: 0 auto 50px;
+  color: #939393;
+  font-size: 18px;
+}
+.sign .main h4 a,
+.sign .main h4 b {
+  padding: 10px;
+  color: #969696;
+  font-weight: 400;
+}
+.sign .main h4 b {
+  font-weight: 700;
+}
+.sign .main h4 a.active {
+  font-weight: 700;
+  color: #ea6f5a;
+  border-bottom: 2px solid #ea6f5a;
+}
+.sign .main h4 a:hover {
+  border-bottom: 2px solid #ea6f5a;
+}
+.sign .main form {
+  color: #969696;
+  margin: 0 0 30px;
+}
+.sign .main form .input-prepend {
+  position: relative;
+  width: 100%;
+}
+.sign .main form .input-prepend input {
+  width: 100%;
+  height: 50px;
+  padding: 4px 12px 4px 30px;
+  border: 1px solid #c8c8c8;
+  border-bottom: 0;
+  background-color: rgb(248, 248, 248);
+}
+.sign .main form .input-prepend:first-of-type input {
+  border-radius: 2px 2px 0 0;
+}
+.sign .main form .input-prepend:last-of-type input {
+  border-bottom: 1px solid #c8c8c8;
+  border-radius: 0 0 2px 2px;
+}
+.sign .main form .input-password input {
+  border-bottom: 1px solid #c8c8c8;
+  border-radius: 0 0 2px 2px;
+}
+.sign .main form .input-prepend i {
+  position: absolute;
+  left: 10px;
+  top: 15px;
+  font-size: 20px;
+  color: #969696;
+}
+.sign .main form .remember {
+  float: left;
+  margin: 20px 0;
+}
+.sign .main form .help {
+  float: right;
+  margin: 20px 0;
+  color: #969696;
+}
+.sign .main form .help:hover {
+  color: #333;
+}
+.sign .main form button {
+  width: 100%;
+  margin: 20px 0 0;
+  padding: 9px 18px;
+  text-align: center;
+  color: #fff;
+  border-radius: 25px;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  clear: both;
+  display: block;
+}
+.sign .main form button.sign-in-btn {
+  background-color: #3194d0;
+}
+.sign .main form button.sign-up-btn {
+  background-color: #42c02e;
+}
+.sign .main form button.sign-in-btn:hover {
+  background-color: #187cb7;
+  cursor: pointer;
+}
+.sign .main form button.sign-up-btn:hover {
+  background-color: #3db922;
+  cursor: pointer;
+}
+.sign .main form p.sign-up-msg {
+  font-size: 12px;
+  margin: 15px 0;
+  text-align: center;
+  line-height: 20px;
+  color: #969696;
+}
+.sign .main form .ssm {
+  position: relative;
+  line-height: 36px;
+}
+.sign .main form .send-ssm {
+  position: absolute;
+  right: 10px;
+  top: 7px;
+  width: 100px;
+  height: 36px;
+  border-radius: 20px;
+  color: #fff;
+  background-color: #42c02e;
+  text-align: center;
+}
+.sign .main .more-sign p {
+  position: relative;
+  font-size: 14px;
+  color: #b8b8b8;
+  margin-bottom: 20px;
+}
+.sign .main .more-sign p::before {
+  content: "";
+  width: 60px;
+  border-bottom: 1px solid #b8b8b8;
+  position: absolute;
+  left: 30px;
+  top: 10px;
+}
+.sign .main .more-sign p::after {
+  content: "";
+  width: 60px;
+  border-bottom: 1px solid #b8b8b8;
+  position: absolute;
+  right: 30px;
+  top: 10px;
+}
+.sign .main .more-sign ul {
+  margin-bottom: 10px;
+}
+.sign .main .more-sign ul li {
+  display: inline-block;
+  color: white;
+  margin: 0 5px;
+  border-radius: 50%;
+}
+.sign .main .more-sign ul li a {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  display: block;
+}
+.sign .main .more-sign ul li a i {
+  font-size: 20px;
+  text-align: center;
+}
+.sign .main .more-sign ul li i.weibo {
+  color: #e95244;
+}
+.sign .main .more-sign ul li i.weixin {
+  color: #00bb29;
+}
+.sign .main .more-sign ul li i.qq {
+  color: #498ad5;
+}
+@media (max-width: 768px) {
+  .sign,
+  body {
+    background-color: white;
+  }
+  .sign .logo {
+    display: none;
+  }
+  .sign .main {
+    margin: 0 auto;
+    box-shadow: none;
+  }
+}
+</style>
---
+++ pages/p/_id.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+      <my-header></my-header>
+       <div class="note">
+            <div class="post">
+                   <div class="article">
+                       <h1 class="title">对简书作者要说的四点免费建议</h1>
+                       <div class="author">
+                            <div class="avatar">
+                                <img src="~/assets/img/default-avatar.jpg">
+                            </div>
+                            <div class="info">
+                                 <nuxt-link class="name" to="name">
+                                  世代
+                                 </nuxt-link>
+                                <a href="#" class="btn btn-follow">
+                                    <i class="fa fa-plus"></i>
+                                <span>关注</span>
+                                </a>
+                              <div class="meta">
+                                <span class="publish-time">
+                                    2018.04.12 17:51*
+                                </span>
+                                <span class="wordage">
+                                    字数 1038
+                                </span>
+                                <span class="view-count">
+                                    阅读 20138
+                                </span>
+                                <span class="comment-count">
+                                    评论 181
+                                </span>
+                                <span class="like-count">
+                                    喜欢 673
+                                </span>
+                                <span class="rewards-count">
+                                    赞赏 29
+                                </span>
+                              </div>
+                          </div>
+
+                        </div>
+                            <div class="show-content">
+                                <p>简书版权是为作者服务的。到现在为止我们做得还不错，接近80部作品签约出版了，从4月份开始，陆续会有一些纸书会面世。更让人开心的是，我们的非虚构类作者，有的已经拿到了起印三万册的条件，而虚构类作者，有的拿到了起印一万册的条件。</p>
+                                <p>对于国内的新人作者来说，这绝对是最高的条件。我和我的同事要谢谢你们，是你们的才华和努力让简书版权成为目前国内最好的新人发现和孵化机构。</p>
+                            </div>
+                       </div>
+                            <div class="show-foot">
+                                <nuxt-link class="notebook" to="/note/123">
+                                     <i class="fa fa-book"></i>
+                                    日记本
+                                </nuxt-link>
+                                <div class="copyright">
+                                    © 著作权归作者所有 
+                                </div>
+                            </div>
+                        <!-- 文章底部的作者信息 -->
+                <div class="follow-detail">
+                  <div class="info">
+                      <div class="avatar">
+                          <img src="~/assets/img/default-avatar.jpg" alt="">
+                      </div>
+                      <a href="#" class="btn-follow">
+                          <i class="fa fa-plus"></i>
+                          <span>关注</span>
+                      </a>
+                      <nuxt-link to="/u/123" class="title">
+                        世代
+                      </nuxt-link>
+                      <i class="fa fa-mars"></i>
+                      <p>写了 74603 字，被 2059 人关注，获得了 1971 个喜欢</p>
+                   </div>
+                 </div>
+                 <div class="meta-bottom">
+                    <div class="like">
+                        <div class="like-btn">
+                            <a href="#" class="">喜欢</a>
+                        </div>
+                        <div class="like-num">
+                            <a href="#">673</a>
+                        </div>
+                    </div>
+                    <div class="share">
+                        <a href="#" class="share-btn">
+                            <i class="fa fa-weixin weixin"></i>
+                        </a>
+                        <a href="#" class="share-btn">
+                            <i class="fa fa-weibo weibo"></i>
+                        </a>
+                        <a href="#" class="share-btn">
+                            <i class="fa fa-qq qq"></i>
+                        </a>
+                        <a href="#" class="share-btn more-share">
+                            更多分享
+                        </a>
+                    </div>
+                 </div>
+                 <!-- 留言组件 -->
+                 <my-comment></my-comment>
+                    </div>
+                </div>
+             </div>
+</template>
+<script>
+import myHeader from "~/components/myHeader";
+import myComment from "~/components/myComment";
+export default {
+  head: {
+    title: "简书文章详情页面-创作你的创作",
+    meta: [
+      { charset: "utf-8" },
+      { name: "keywords", content: "文章详情页面" },
+      { name: "description", content: "文章页面" }
+    ]
+  },
+  data() {
+    return {};
+  },
+  components: {
+    myHeader,
+    myComment
+  }
+};
+</script>
